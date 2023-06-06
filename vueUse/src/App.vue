e85af77a04dc15b0b7c837996f152e7e6daecb3c
Katsiaryna <36642433+UlakhovichKate@users.noreply.github.com>
2023-06-06 14:43:44 +0300

    create global state

--- vueUse/src/App.vue
+++ vueUse/src/App.vue
@@ -1,21 +1,12 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <create-global-state></create-global-state>
   </main>
 </template>
+
+<script setup>
+  import createGlobalState from './vueUseComponents/CreateGlobalState.vue'
+</script>
 
 <style scoped>
 header {
---
+++ vueUse/src/vueUseComponents/CreateGlobalState.vue
@@ -0,0 +1,25 @@
+<template>
+    <h2>create global state</h2>
+    <div>
+        <input v-model="state.name" type="text">
+        <input v-model="state.color" type="text">
+        <input v-model="state.size" type="text">
+
+        <pre>{{ text }} </pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+    import { createGlobalState, useStorage } from '@vueuse/core'
+
+    const useState = createGlobalState(() =>
+        useStorage('vue-use-locale-storage', {
+            name: 'Banana',
+            color: 'Yellow',
+            size: 'Medium',
+        }),
+    );
+
+    const state = useState();
+    const text = state
+</script>
