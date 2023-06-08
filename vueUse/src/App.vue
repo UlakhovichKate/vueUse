2318563d251bfdddd7b763a9f1e1a1fa11cd12fc
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2023-06-08 10:17:16 +0300

    UseCssVar.vue

--- vueUse/src/App.vue
+++ vueUse/src/App.vue
@@ -33,6 +33,7 @@
     <use-browser-location/>
     <use-clipboard/>
     <use-color-mode/>
+    <use-css-var/>
   </main>
 </template>
 
@@ -68,35 +69,14 @@
   import UseBrowserLocation from "./vueUseComponents/UseBrowserLocation.vue";
   import UseClipboard from "./vueUseComponents/UseClipboard.vue";
   import UseColorMode from "./vueUseComponents/UseColorMode.vue";
+  import UseCssVar from "./vueUseComponents/UseCssVar.vue";
+
 
 
 </script>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style>
+h2 {
+  margin-top: 50px;
 }
 </style>
