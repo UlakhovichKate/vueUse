2a9f189a4bb55f8bb6adebaadd580796f560b2e5
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2023-06-09 12:47:58 +0300

    OnStartTyping.vue

--- vueUse/src/components/ListSensors.vue
+++ vueUse/src/components/ListSensors.vue
@@ -2,12 +2,15 @@
   <on-click-outside/>
   <on-key-stroke/>
   <on-long-press/>
+  <on-start-typing/>
 </template>
 
 <script setup>
 import OnClickOutside from '../vueUseComponents/OnClickOutside.vue'
 import OnKeyStroke from "../vueUseComponents/OnKeyStroke.vue";
 import OnLongPress from "../vueUseComponents/OnLongPress.vue";
+import OnStartTyping from "../vueUseComponents/OnStartTyping.vue";
+
 
 </script>
 
---
+++ vueUse/src/vueUseComponents/OnStartTyping.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { onStartTyping } from '@vueuse/core'
+
+const input = ref<HTMLInputElement | null>(null)
+
+onStartTyping(() => {
+  if (input.value !== document.activeElement)
+    input.value!.focus()
+})
+</script>
+
+<template>
+  <h2>On Start Typing</h2>
+  <p>Type anything</p>
+  <input ref="input" type="text" placeholder="Start typing to focus">
+  <input type="text" placeholder="Start typing has no effect here">
+</template>
