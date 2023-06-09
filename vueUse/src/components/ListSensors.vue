f4024451dc1c2ff4787466bdce4eebc951448601
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2023-06-09 12:20:59 +0300

    OnKeyStroke.vue

--- vueUse/src/components/ListSensors.vue
+++ vueUse/src/components/ListSensors.vue
@@ -1,9 +1,12 @@
 <template>
   <on-click-outside/>
+  <on-key-stroke/>
 </template>
 
 <script setup>
 import OnClickOutside from '../vueUseComponents/OnClickOutside.vue'
+import OnKeyStroke from "../vueUseComponents/OnKeyStroke.vue";
+
 
 </script>
 
