a68ded001348efd510a858f4c111f442af94057f
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2023-06-14 12:41:44 +0300

    UseRafFn.vue

--- src/components/ListAnimation.vue
+++ src/components/ListAnimation.vue
@@ -3,6 +3,7 @@
   <use-interval/>
   <use-interval-fn/>
   <use-now/>
+  <use-raf-fn/>
 </template>
 
 <script setup>
@@ -10,7 +11,7 @@ import UseAnimate from "../vueUseComponents/UseAnimate.vue";
 import UseInterval from "../vueUseComponents/UseInterval.vue";
 import UseIntervalFn from "../vueUseComponents/UseIntervalFn.vue";
 import UseNow from "../vueUseComponents/UseNow.vue";
-
+import UseRafFn from "../vueUseComponents/UseRafFn.vue";
 
 </script>
 
---
+++ src/vueUseComponents/UseRafFn.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>Use Raf Fn</h2>
+  <div>{{ count }}</div>
+  <button type="button" class="btn btn-danger" @click="pause">Pause</button>
+  <button type="button" class="btn btn-danger" @click="resume">Resume</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useRafFn } from '@vueuse/core'
+
+const count = ref(0)
+
+const { pause, resume } = useRafFn(() => {
+  count.value++
+})
+</script>
+
+<style scoped>
+
+</style>
