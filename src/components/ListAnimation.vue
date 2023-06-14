ae6afe81e9f9e45a373a530fecf21abe85d34759
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2023-06-14 12:37:03 +0300

    UseIntervalFn.vue

--- src/components/ListAnimation.vue
+++ src/components/ListAnimation.vue
@@ -1,11 +1,14 @@
 <template>
   <use-animate/>
   <use-interval/>
+  <use-interval-fn/>
 </template>
 
 <script setup>
 import UseAnimate from "../vueUseComponents/UseAnimate.vue";
 import UseInterval from "../vueUseComponents/UseInterval.vue";
+import UseIntervalFn from "../vueUseComponents/UseIntervalFn.vue";
+
 
 </script>
 
---
+++ src/vueUseComponents/UseIntervalFn.vue
@@ -0,0 +1,26 @@
+<template>
+  <h2>Use Intervel Fn</h2>
+  <div>{{ el }}</div>
+  <button v-if="isActive" class="btn btn-danger" @click="pause">
+    Pause
+  </button>
+  <button v-if="!isActive" @click="resume"  class="btn btn-danger">
+    Resume
+  </button>
+</template>
+
+<script setup>
+import {rand, useIntervalFn} from '@vueuse/core'
+import {ref} from "vue";
+
+let arr = ['apple', 'grapes', 'orange', 'watermelon']
+const el = ref('');
+
+const { pause, resume, isActive } = useIntervalFn(() => {
+  el.value = arr[rand(0, arr.length - 1)]
+}, 1000)
+</script>
+
+<style scoped>
+
+</style>
