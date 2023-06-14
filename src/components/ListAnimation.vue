8275af57516085a012ef3a4b38ba8db34ca5e24b
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2023-06-14 12:44:26 +0300

    UseTimeout.vue

--- src/components/ListAnimation.vue
+++ src/components/ListAnimation.vue
@@ -4,6 +4,7 @@
   <use-interval-fn/>
   <use-now/>
   <use-raf-fn/>
+  <use-timeout/>
 </template>
 
 <script setup>
@@ -12,6 +13,7 @@ import UseInterval from "../vueUseComponents/UseInterval.vue";
 import UseIntervalFn from "../vueUseComponents/UseIntervalFn.vue";
 import UseNow from "../vueUseComponents/UseNow.vue";
 import UseRafFn from "../vueUseComponents/UseRafFn.vue";
+import UseTimeout from "../vueUseComponents/UseTimeout.vue";
 
 </script>
 
---
+++ src/vueUseComponents/UseTimeout.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>Use Timeout</h2>
+  <div>Wait 1000ms {{ ready }}</div>
+</template>
+
+<script setup>
+import { promiseTimeout, useTimeout } from '@vueuse/core'
+
+const ready = useTimeout(1000)
+</script>
+
+<style scoped>
+
+</style>
