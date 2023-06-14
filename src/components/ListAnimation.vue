cb10b86a2eed8d3fde2f0e0e85b9c2d1d3dcde8f
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2023-06-14 12:27:59 +0300

    UseInterval.vue

--- src/components/ListAnimation.vue
+++ src/components/ListAnimation.vue
@@ -1,9 +1,11 @@
 <template>
   <use-animate/>
+  <use-interval/>
 </template>
 
 <script setup>
 import UseAnimate from "../vueUseComponents/UseAnimate.vue";
+import UseInterval from "../vueUseComponents/UseInterval.vue";
 
 </script>
 
---
+++ src/vueUseComponents/UseInterval.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2>Use Interval</h2>
+  <div>{{ counter }}</div>
+</template>
+
+<script setup>
+import { useInterval } from '@vueuse/core'
+
+// count will increase every 200ms
+const counter = useInterval(200)
+</script>
+
+<style scoped>
+
+</style>
