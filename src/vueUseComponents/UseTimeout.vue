949a4c629e2a93d5adf1b2fd2a41a7388f0df7a5
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2023-06-14 12:45:02 +0300

    UseTimeout.vue

--- src/vueUseComponents/UseTimeout.vue
+++ src/vueUseComponents/UseTimeout.vue
@@ -1,12 +1,12 @@
 <template>
   <h2>Use Timeout</h2>
-  <div>Wait 1000ms {{ ready }}</div>
+  <div>Wait 10s: {{ ready }}</div>
 </template>
 
 <script setup>
 import { promiseTimeout, useTimeout } from '@vueuse/core'
 
-const ready = useTimeout(1000)
+const ready = useTimeout(10000)
 </script>
 
 <style scoped>
