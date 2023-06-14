f6a1a208670bc95ad6c8db0e657acd704c93f4cc
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2023-06-14 13:44:59 +0300

    CreateReusableTemplate.vue

--- src/components/ListComponents.vue
+++ src/components/ListComponents.vue
@@ -1,9 +1,12 @@
 <template>
   <computed-inject/>
+  <create-reusable-template/>
 </template>
 
 <script setup>
 import ComputedInject from "../vueUseComponents/ComputedInject.vue";
+import CreateReusableTemplate from "../vueUseComponents/CreateReusableTemplate.vue";
+
 
 </script>
 
---
+++ src/vueUseComponents/CreateReusableTemplate.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { createReusableTemplate } from '@vueuse/core'
+
+let data = 'First line';
+let anotherData = 'Second line';
+let something = 'Third line';
+
+const [DefineTemplate, ReuseTemplate] = createReusableTemplate()
+</script>
+
+<template>
+  <h2>Create Reusable Template</h2>
+  <DefineTemplate v-slot="{ data, msg, anything }">
+    <div>{{ data }} passed from usage. {{ msg }}</div>
+  </DefineTemplate>
+
+  <ReuseTemplate :data="data" msg="The first usage" />
+  <ReuseTemplate :data="anotherData" msg="The second usage" />
+  <ReuseTemplate v-bind="{ data: something, msg: 'The third' }" />
+</template>
