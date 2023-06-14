ea8fd57d7c429aceb707ddee59057a8cd4d7f230
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2023-06-14 12:39:12 +0300

    UseNow.vue

--- src/components/ListAnimation.vue
+++ src/components/ListAnimation.vue
@@ -2,12 +2,14 @@
   <use-animate/>
   <use-interval/>
   <use-interval-fn/>
+  <use-now/>
 </template>
 
 <script setup>
 import UseAnimate from "../vueUseComponents/UseAnimate.vue";
 import UseInterval from "../vueUseComponents/UseInterval.vue";
 import UseIntervalFn from "../vueUseComponents/UseIntervalFn.vue";
+import UseNow from "../vueUseComponents/UseNow.vue";
 
 
 </script>
---
+++ src/vueUseComponents/UseNow.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>Use Now</h2>
+  <div>{{ now }}</div>
+</template>
+
+<script setup>
+import { useNow } from '@vueuse/core'
+
+const now = useNow()
+</script>
+
+<style scoped>
+
+</style>
