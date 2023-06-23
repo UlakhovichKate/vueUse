486747024682684ca9261479b6367f3d7176b012
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2023-06-23 16:34:43 +0300

    UseArrayFindLast.vue

--- src/components/ListArray.vue
+++ src/components/ListArray.vue
@@ -1,9 +1,11 @@
 <template>
   <use-array-difference/>
+  <use-array-find-last/>
 </template>
 
 <script setup>
 import UseArrayDifference from "../vueUseComponents/UseArrayDifference.vue";
+import UseArrayFindLast from "../vueUseComponents/UseArrayFindLast.vue";
 
 </script>
 
---
+++ src/vueUseComponents/UseArrayFindLast.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>Use Array Find Last</h2>
+  <p>list: {{ list }}</p>
+  <p>last one: {{ positive }}</p>
+</template>
+
+<script setup>
+import { useArrayFindLast } from '@vueuse/core'
+import { ref } from 'vue';
+
+const list = [ref(1), ref(-1), ref(2)]
+const positive = useArrayFindLast(list, val => val > 0)
+// positive.value: 2
+</script>
+
+<style scoped>
+
+</style>
