b0d0a3ca02b45ba5f08fba8b37106c0d5c97beb2
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2023-06-23 16:12:01 +0300

    SyncRef.vue

--- src/components/ListReactivity.vue
+++ src/components/ListReactivity.vue
@@ -1,11 +1,13 @@
 <template>
   <reactify-object/>
   <reactive-omit/>
+  <sync-ref/>
 </template>
 
 <script setup>
 import ReactifyObject from "../vueUseComponents/ReactifyObject.vue";
 import ReactiveOmit from "../vueUseComponents/ReactiveOmit.vue";
+import SyncRef from "../vueUseComponents/SyncRef.vue";
 
 
 </script>
---
+++ src/vueUseComponents/SyncRef.vue
@@ -0,0 +1,19 @@
+<template>
+  <h2>Sync Ref</h2>
+  <input v-model="a" placeholder="A" type="text"><br>
+  <input v-model="b" placeholder="B" type="text">
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { syncRef } from '@vueuse/core'
+
+const a = ref('')
+const b = ref('')
+
+syncRef(a, b)
+</script>
+
+<style scoped>
+
+</style>
