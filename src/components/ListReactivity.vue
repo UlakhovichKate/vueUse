d27441f9c2d9eb0042f12c97bc05ae4dcf39b3b8
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2023-06-23 16:19:42 +0300

    ToReactive.vue

--- src/components/ListReactivity.vue
+++ src/components/ListReactivity.vue
@@ -2,12 +2,15 @@
   <reactify-object/>
   <reactive-omit/>
   <sync-ref/>
+  <to-reactive/>
 </template>
 
 <script setup>
 import ReactifyObject from "../vueUseComponents/ReactifyObject.vue";
 import ReactiveOmit from "../vueUseComponents/ReactiveOmit.vue";
 import SyncRef from "../vueUseComponents/SyncRef.vue";
+import ToReactive from "../vueUseComponents/ToReactive.vue";
+
 
 
 </script>
---
+++ src/vueUseComponents/ToReactive.vue
@@ -0,0 +1,27 @@
+<template>
+  <h2>To Reactive</h2>
+  <p>console.log</p>
+</template>
+
+<script setup>
+import { toReactive } from '@vueuse/core'
+import { ref } from 'vue';
+
+const refState = ref({ foo: 'bar' })
+
+console.log(refState.value.foo) // => 'bar'
+
+const state = toReactive(refState) // <--
+
+console.log(state.foo) // => 'bar'
+
+refState.value = { bar: 'foo' }
+
+console.log(state.foo) // => undefined
+console.log(state.bar) // => 'foo'
+console.log(refState.value)
+</script>
+
+<style scoped>
+
+</style>
