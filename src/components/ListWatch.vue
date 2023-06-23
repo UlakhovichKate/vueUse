22285e210557a90c245d676fbec41d98f04b1189
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2023-06-23 10:49:33 +0300

    WatchDebounced.vue

--- src/components/ListWatch.vue
+++ src/components/ListWatch.vue
@@ -2,13 +2,14 @@
   <until/>
   <watch-array/>
   <watch-at-most/>
+  <watch-debounced/>
 </template>
 
 <script setup>
 import Until from "../vueUseComponents/Until.vue";
 import WatchArray from "../vueUseComponents/WatchArray.vue";
 import WatchAtMost from "../vueUseComponents/WatchAtMost.vue";
-
+import WatchDebounced from "../vueUseComponents/WatchDebounced.vue";
 
 
 </script>
---
+++ src/vueUseComponents/WatchDebounced.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { watchDebounced } from '@vueuse/core'
+
+const input = ref('')
+const updated = ref(0)
+
+watchDebounced(input, () => {
+  updated.value += 1
+}, { debounce: 1000, maxWait: 5000 })
+</script>
+
+<template>
+  <h2>Watch Debounced</h2>
+  <div>
+    <input v-model="input" placeholder="Try to type anything..." type="text">
+    <p>Delay is set to 1000ms and maxWait is set to 5000ms for this demo.</p>
+
+    <p>Input: {{ input }}</p>
+    <p>Times Updated: {{ updated }}</p>
+  </div>
+</template>
