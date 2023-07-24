e125f008e9e74b3af55fc09c3305a21d13df5875
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2023-07-24 11:01:17 +0300

    Get.vue

--- src/components/ListUtilities.vue
+++ src/components/ListUtilities.vue
@@ -1,10 +1,11 @@
 <template>
   <create-unref-fn/>
+  <get/>
 </template>
 
 <script setup>
 import CreateUnrefFn from "../vueUseComponents/CreateUnrefFn.vue";
-
+import Get from "../vueUseComponents/Get.vue";
 
 
 </script>
---
+++ src/vueUseComponents/Get.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>Get</h2>
+  <pre>const a = ref(42)</pre>
+  (console.log)
+</template>
+
+<script setup>
+import { get } from '@vueuse/core'
+import {ref} from "vue";
+
+const a = ref(42)
+
+console.log(get(a)) // 42
+</script>
+
+<style scoped>
+
+</style>
