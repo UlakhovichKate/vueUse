11bf15f6aaebc644cfaa679650d1188c73780e77
Ulakhovich Katsiaryna <e.ulahovich@belitsoft.com>
2023-07-24 11:45:03 +0300

    UseCached.vue

--- src/components/ListUtilities.vue
+++ src/components/ListUtilities.vue
@@ -5,6 +5,7 @@
   <is-defined/>
   <use-async-queue/>
   <use-base64/>
+  <use-cached/>
 </template>
 
 <script setup>
@@ -14,7 +15,7 @@ import IsDefined from "../vueUseComponents/IsDefined.vue";
 import UtiSet from "../vueUseComponents/UtiSet.vue";
 import UseAsyncQueue from "../vueUseComponents/UseAsyncQueue.vue";
 import UseBase64 from "../vueUseComponents/UseBase64.vue";
-
+import UseCached from "../vueUseComponents/UseCached.vue";
 
 
 </script>
